02e6161df0e70f79d1ec1019ea08f37d2d2c38fd
Alexey Trukhlyayev <exact84@gmail.com>
2025-07-19 01:39:51 +0500

    fix: Aplly all unicorn rules

--- smart-home-ui/eslint.config.js
+++ smart-home-ui/eslint.config.js
@@ -1,17 +1,23 @@
-const eslint = require('@eslint/js');
-const tseslint = require('typescript-eslint');
-const angular = require('angular-eslint');
-const unicorn = require('eslint-plugin-unicorn').default;
-const prettier = require('eslint-config-prettier');
-const eslintPluginPrettier = require('eslint-plugin-prettier');
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+const unicorn = require("eslint-plugin-unicorn").default;
+const prettier = require("eslint-config-prettier");
+const eslintPluginPrettier = require("eslint-plugin-prettier");
 
 module.exports = tseslint.config(
   {
-    files: ['**/*.ts'],
+    files: ["**/*.ts"],
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.recommended,
       ...angular.configs.tsRecommended,
+      {
+        plugins: {
+          unicorn,
+        },
+        rules: unicorn.configs.recommended.rules,
+      },
       prettier,
     ],
     plugins: {
@@ -19,24 +25,24 @@ module.exports = tseslint.config(
       prettier: eslintPluginPrettier,
     },
     rules: {
-      '@angular-eslint/component-selector': [
-        'error',
-        { type: 'element', prefix: 'app', style: 'kebab-case' },
+      "@angular-eslint/component-selector": [
+        "error",
+        { type: "element", prefix: "app", style: "kebab-case" },
       ],
-      '@angular-eslint/directive-selector': [
-        'error',
-        { type: 'attribute', prefix: 'app', style: 'camelCase' },
+      "@angular-eslint/directive-selector": [
+        "error",
+        { type: "attribute", prefix: "app", style: "camelCase" },
       ],
-      'unicorn/better-regex': 'error',
-      'unicorn/no-empty-file': 'error',
-      'unicorn/prevent-abbreviations': 'off',
-      'unicorn/no-null': 'error',
-      'unicorn/filename-case': 'off',
-      'prettier/prettier': 'error',
+      "unicorn/better-regex": "error",
+      "unicorn/no-empty-file": "error",
+      "unicorn/prevent-abbreviations": "off",
+      "unicorn/no-null": "error",
+      "unicorn/filename-case": "off",
+      "prettier/prettier": "error",
     },
   },
   {
-    files: ['**/*.html'],
+    files: ["**/*.html"],
     extends: [...angular.configs.templateRecommended],
-  }
+  },
 );
