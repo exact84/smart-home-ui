b925c42c797861ecf67518a0d81cecee23f4c00f
Alexey Trukhlyayev <exact84@gmail.com>
2025-08-02 12:37:21 +0500

    refactor: exclude effect from highlightActive directive

--- smart-home-ui/src/app/directives/highlight-active.ts
+++ smart-home-ui/src/app/directives/highlight-active.ts
@@ -1,11 +1,4 @@
-import {
-  Directive,
-  effect,
-  ElementRef,
-  inject,
-  Input,
-  signal,
-} from '@angular/core';
+import { Directive, ElementRef, inject, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightActive]',
@@ -13,15 +6,7 @@ import {
 })
 export class HighlightActive {
   private el = inject(ElementRef<HTMLElement>);
-  private conditionSignal = signal(false);
-
-  private readonly applyEffect = effect(this.toggleActiveBorder.bind(this));
-
-  private toggleActiveBorder(): void {
-    const isActive = this.conditionSignal();
-    this.el.nativeElement.classList.toggle('active-border', isActive);
-  }
   @Input('appHighlightActive') set condition(value: boolean) {
-    this.conditionSignal.set(value);
+    this.el.nativeElement.classList.toggle('active-border', value);
   }
 }
